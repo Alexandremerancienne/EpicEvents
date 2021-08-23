5a662a209c73d81c1760c1ce13924afb0c325265
Alexandre Merancienne <alexandre.merancienne@gmail.com>
2021-08-23 17:24:04 +0300

    Filters factorized

--- crm/filters.py
+++ crm/filters.py
@@ -7,9 +7,8 @@ from crm.models import Client, Contract, Event, Note
 class UserFilter(filters.FilterSet):
     """Implements filters to be used with UserViewSet."""
 
-    username_contains = filters.CharFilter(
-        field_name="username", lookup_expr="icontains"
-    )
+    username_contains = filters.CharFilter(field_name="username",
+                                           lookup_expr="icontains")
 
     class Meta:
         model = User
@@ -19,24 +18,19 @@ class UserFilter(filters.FilterSet):
 class ClientFilter(filters.FilterSet):
     """Implements filters to be used with ClientViewSet."""
 
-    first_name__contains = filters.CharFilter(
-        field_name="first_name", lookup_expr="icontains"
-    )
+    first_name__contains = filters.CharFilter(field_name="first_name",
+                                              lookup_expr="icontains")
     first_name = filters.CharFilter(field_name="first_name",
                                     lookup_expr="iexact")
     last_name = filters.CharFilter(field_name="last_name",
                                    lookup_expr="iexact")
-    company = filters.CharFilter(field_name="company", lookup_expr="iexact")
+    company = filters.CharFilter(field_name="company",
+                                 lookup_expr="iexact")
 
     class Meta:
         model = Client
-        fields = [
-            "first_name",
-            "first_name__contains",
-            "last_name",
-            "company",
-            "sales_contact",
-        ]
+        fields = ["first_name", "first_name__contains",
+                  "last_name", "company", "sales_contact"]
 
 
 class ContractFilter(filters.FilterSet):
@@ -58,9 +52,8 @@ class EventFilter(filters.FilterSet):
 class NoteFilter(filters.FilterSet):
     """Implements filters to be used with NoteViewSet."""
 
-    description__contains = filters.CharFilter(
-        field_name="description", lookup_expr="icontains"
-    )
+    description__contains = filters.CharFilter(field_name="description",
+                                               lookup_expr="icontains")
 
     class Meta:
         model = Note
